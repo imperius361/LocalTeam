625ad263b90a38abee614e71a39bfff00fe4d5b9
Clae <clae.diender@gmail.com>
2026-03-21 13:56:54 +1000

    fix: remove stale index.css import (file never existed in this project)

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import './themes/obsidian.css';
 import './themes/pixel.css';
 import { ThemeProvider } from './themes/ThemeContext';
